2dca2bd95acf234c8dc5ca8cac68a9dafbab454c
David vG <naninoiza@outlook.com>
2026-08-12 13:22:16 +0200

    added a new class for basics recap and started recap with variables

---
+++ LearningAndTesting/DotNetRecap/DotNetBasics.cs
@@ -0,0 +1,15 @@
+﻿namespace LearningAndTesting.DotNetRecap;
+
+public class DotNetBasics
+{
+    // Fields, variables and properties
+    private int _notAccessibleField = 1; // Field --> class variable --> _camelCase
+    public int AccessibleProperty { get; set; } = 2; // Property --> public class variable --> PascalCase
+    // Note: { get; set; } get: necessary to retrieve val from outside, set: necessary to change val from outside
+    
+    public int GetNotAccessibleField()
+    {
+        var notAccessibleVariable = _notAccessibleField; // Variable --> camelCase
+        return notAccessibleVariable;
+    }
+}
--- LearningAndTesting/Program.cs
+++ LearningAndTesting/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Includes
+
+// Hello World
+
+using LearningAndTesting.DotNetRecap;
+
+Console.WriteLine("Hello, World!");
+
+// Create an instance of DotNetBasics
+var basics = new DotNetBasics();
+
+// Fields, variables and Properties
+var retrievedProperty = basics.AccessibleProperty;
+Console.WriteLine($"Property Value: {retrievedProperty}");
+
+var retrievedPrivate = basics.GetNotAccessibleField();
+Console.WriteLine($"Function-Return Value: {retrievedPrivate}");
+
